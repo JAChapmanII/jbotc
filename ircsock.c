e3ec5483ec45800a9bda41465934ee9df8c4ecf9
Jeff Chapman II <jac@JAChapmanII.net>
2011-09-24 14:44:27 -0400

    Make IRCSocket mostly functional
    We construct a CBuffer when we construct a IRCSock. _connect calls the newly
    implemented function to lookup a the IP address of a domain name. This needs
    to be fixed to use the supplied port. _read attempts to properly parse the
    reads from the socket into lines, which it then adds to cbuf. _send needs
    error handling but does send messages.
    int ircsock_connect(IRCSock *ircsock);
    void ircsock_read(IRCSock *ircsock);
    void ircsock_send(IRCSock *ircsock, char *str);

--- ircsock.c
+++ ircsock.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <errno.h>
+
 IRCSock *ircsock_create(char *host, int port, char *nick, char *chan) {
 	IRCSock *ircsock = malloc(sizeof(IRCSock));
 	if(!ircsock)
@@ -10,6 +18,7 @@ IRCSock *ircsock_create(char *host, int port, char *nick, char *chan) {
 	ircsock->host = NULL;
 	ircsock->nick = NULL;
 	ircsock->chan = NULL;
+	ircsock->cbuf = NULL;
 
 	ircsock->host = malloc(strlen(host) + 1);
 	if(!ircsock->host) {
@@ -19,6 +28,7 @@ IRCSock *ircsock_create(char *host, int port, char *nick, char *chan) {
 	strcpy(ircsock->host, host);
 
 	ircsock->port = port;
+	ircsock->domain = -1;
 
 	ircsock->nick = malloc(strlen(nick) + 1);
 	if(!ircsock->nick) {
@@ -33,16 +43,89 @@ IRCSock *ircsock_create(char *host, int port, char *nick, char *chan) {
 		return NULL;
 	}
 	strcpy(ircsock->chan, chan);
-	
+
+	ircsock->socket = -1;
+
+	ircsock->cbuf = cbuffer_create(4096);
+
 	return ircsock;
 }
 
 void ircsock_free(IRCSock *ircsock) {
 	if(!ircsock)
 		return;
+	cbuffer_free(ircsock->cbuf);
 	free(ircsock->host);
 	free(ircsock->nick);
 	free(ircsock->chan);
 	free(ircsock);
+}
+
+struct addrinfo *ircsock_lookupDomain(IRCSock *ircsock) {
+	struct addrinfo *result;
+	int error;
+
+	if(ircsock->socket == -1) {
+		printf("ircsock->socket == -1!\n");
+		return NULL;
+	}
+
+	error = getaddrinfo(ircsock->host, "6667", NULL, &result);
+	if(error) {
+		printf("could not getaddrinfo!\n");
+		return NULL;
+	}
+	return result;
+}
+
+int ircsock_connect(IRCSock *ircsock) {
+	struct addrinfo *result;
+	int error;
+
+	ircsock->socket = socket(AF_INET, SOCK_STREAM, 0);
+	if(ircsock->socket == -1) {
+		printf("Could not create ircsock->socket!\n");
+		return 0;
+	}
+
+	result = ircsock_lookupDomain(ircsock);
+	if(!result) {
+		printf("Could not lookup domain!\n");
+		return 0;
+	}
+	error = connect(ircsock->socket, result->ai_addr, result->ai_addrlen);
+	if(error == -1) {
+		printf("connect call failed! %d\n", error);
+		return 0;
+	}
+	freeaddrinfo(result);
+
+	return 1;
+}
+
+void ircsock_read(IRCSock *ircsock) {
+	char *buf = malloc(4096);
+	ssize_t ramount;
+	char *str, *tok;
+
+	ramount = read(ircsock->socket, buf, 4096);
+	if(ramount > 0) {
+		buf[ramount] = '\n';
+		buf[ramount + 1] = '\0';
+		tok = strtok(buf, "\r\n");
+		while(tok != NULL) {
+			str = malloc(strlen(tok) + 1);
+			strcpy(str, tok);
+
+			cbuffer_push(ircsock->cbuf, str);
+			tok = strtok(NULL, "\r\n");
+		}
+	}
+	free(buf);
+}
+
+void ircsock_send(IRCSock *ircsock, char *str) {
+	write(ircsock->socket, str, strlen(str));
+	write(ircsock->socket, "\r\n", 2);
 }
 
--- ircsock.h
+++ ircsock.h
@@ -6,14 +6,21 @@
 typedef struct {
 	char *host;
 	int port;
+	int domain;
 
 	char *nick;
 	char *chan;
+
+	int socket;
 
 	CBuffer *cbuf;
 } IRCSock;
 
 IRCSock *ircsock_create(char *host, int port, char *nick, char *chan);
 void ircsock_free(IRCSock *ircsock);
+
+int ircsock_connect(IRCSock *ircsock);
+void ircsock_read(IRCSock *ircsock);
+void ircsock_send(IRCSock *ircsock, char *str);
 
 #endif /* IRCSOCK_H */
