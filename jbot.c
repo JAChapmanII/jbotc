ce428fdc7dcf2ab252ae586cc727f76e25763c6f
Jeff Chapman II <jac@JAChapmanII.net>
2011-09-24 15:10:59 -0400

    Replace join code with call to ircsock_join

--- jbot.c
+++ jbot.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include "ircsock.h"
 
 #define SERVER ".slashnet.org"
@@ -10,7 +9,8 @@ IRCSock *ircSocket = NULL;
 
 int main(int argc, char **argv) {
 	char *prefix = "irc", *str, *sname;
-	int foundPing = 0;
+	int port = 6667;
+	char *nick = "jbotc", *chan = "#zebra";
 	if(argc > 1)
 		prefix = argv[1];
 
@@ -20,7 +20,7 @@ int main(int argc, char **argv) {
 	printf("Server: %s\n", sname);
 
 	printf("Creating ircSocket...\n");
-	ircSocket = ircsock_create(sname, 6667, "jbotc", "#zebra");
+	ircSocket = ircsock_create(sname, port, nick, chan);
 	if(!ircSocket) {
 		fprintf(stderr, "Could not create ircSocket!\n");
 		return 1;
@@ -31,34 +31,10 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	printf("Sending nick/user to server...\n");
-	ircsock_send(ircSocket, "NICK jbotc");
-	ircsock_send(ircSocket, "USER jbotc j j :jbotc");
-	sleep(1);
+	printf("Joining %s as %s...\n", chan, nick);
+	ircsock_join(ircSocket);
 
-	printf("Ping loop\n");
-	while(!foundPing) {
-		ircsock_read(ircSocket);
-		while((str = cbuffer_pop(ircSocket->cbuf)) != NULL) {
-			printf("%s\n", str);
-			if((str[0] == 'P') || (str[1] == 'I') ||
-				(str[2] == 'N') || (str[3] == 'G') ||
-				(str[4] == ' ') || (str[5] == ':')) {
-				str[1] = 'O';
-				printf(" -> %s\n", str);
-				ircsock_send(ircSocket, str);
-				free(str);
-
-				foundPing = 1;
-				break;
-			}
-		}
-	}
-
-	printf("Joining #zebra\n");
-	ircsock_send(ircSocket, "JOIN #zebra");
-
-	printf("main loop\n");
+	printf("Main loop\n");
 	while(1) {
 		ircsock_read(ircSocket);
 		while((str = cbuffer_pop(ircSocket->cbuf)) != NULL) {
