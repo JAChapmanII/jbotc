521814affee8a71454b02c67a5bb30e0dfb900ff
Jeff Chapman II <jac@JAChapmanII.net>
2011-09-24 16:15:48 -0400

    Use regex's to prase out PRIVMSGs

--- jbot.c
+++ jbot.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <regex.h>
 #include "ircsock.h"
 
 #define SERVER ".slashnet.org"
@@ -16,11 +17,30 @@ void *readLoop(void *args) {
 	}
 }
 
+char *getRegError(int errcode, regex_t *compiled) {
+	size_t l = regerror(errcode, compiled, NULL, 0);
+	char *buffer = malloc(l + 1);
+	regerror(errcode, compiled, buffer, l);
+	return buffer;
+}
+
 int main(int argc, char **argv) {
 	char *nick = "jbotc", *chan = "#zebra";
 	char *prefix = "irc", *str, *sname;
 	int port = 6667;
 	pthread_t readThread;
+	regex_t *pmsgRegex = malloc(sizeof(regex_t));
+	regmatch_t mptr[4];
+	int res;
+
+	res = regcomp(pmsgRegex,
+			"([A-Za-z0-9_]*)!([-@~A-Za-z0-9_\\.]*) PRIVMSG (#[A-Za-z0-9_]*) :(.*)",
+			REG_EXTENDED);
+	if(res) {
+		fprintf(stderr, "Could not compile regex!\n");
+		fprintf(stderr, "erromsg: %s\n", getRegError(res, pmsgRegex));
+		return 1;
+	}
 
 	if(argc > 1)
 		prefix = argv[1];
@@ -51,7 +71,6 @@ int main(int argc, char **argv) {
 	printf("Main loop\n");
 	while(1) {
 		while((str = cbuffer_pop(ircSocket->cbuf)) != NULL) {
-			printf("%s\n", str);
 			if((str[0] == 'P') && (str[1] == 'I') &&
 				(str[2] == 'N') && (str[3] == 'G') &&
 				(str[4] == ' ') && (str[5] == ':')) {
@@ -59,8 +78,15 @@ int main(int argc, char **argv) {
 				str[1] = 'O';
 				printf(" -> %s\n", str);
 				ircsock_send(ircSocket, str);
-				free(str);
+			} else if((res = regexec(pmsgRegex, str, pmsgRegex->re_nsub + 1, mptr, 0)) == 0) {
+				printf("PRIVMSG recieved from %.*s@%.*s: %.*s\n",
+						mptr[1].rm_eo - mptr[1].rm_so, str + mptr[1].rm_so,
+						mptr[3].rm_eo - mptr[3].rm_so, str + mptr[3].rm_so,
+						mptr[4].rm_eo - mptr[4].rm_so, str + mptr[4].rm_so);
+			} else {
+				printf("%s\n", str);
 			}
+			free(str);
 		}
 	}
 
