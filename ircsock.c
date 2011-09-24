0e9f46227d8604f6603f7c892c7800b8326ce7b8
Jeff Chapman II <jac@JAChapmanII.net>
2011-09-24 10:25:58 -0400

    Flesh out IRCSock type more, implement _create and _free

--- ircsock.c
+++ ircsock.c
@@ -1,2 +1,48 @@
 #include "ircsock.h"
+#include <stdlib.h>
+#include <string.h>
+
+IRCSock *ircsock_create(char *host, int port, char *nick, char *chan) {
+	IRCSock *ircsock = malloc(sizeof(IRCSock));
+	if(!ircsock)
+		return NULL;
+
+	ircsock->host = NULL;
+	ircsock->nick = NULL;
+	ircsock->chan = NULL;
+
+	ircsock->host = malloc(strlen(host) + 1);
+	if(!ircsock->host) {
+		ircsock_free(ircsock);
+		return NULL;
+	}
+	strcpy(ircsock->host, host);
+
+	ircsock->port = port;
+
+	ircsock->nick = malloc(strlen(nick) + 1);
+	if(!ircsock->nick) {
+		ircsock_free(ircsock);
+		return NULL;
+	}
+	strcpy(ircsock->nick, nick);
+
+	ircsock->chan = malloc(strlen(chan) + 1);
+	if(!ircsock->chan) {
+		ircsock_free(ircsock);
+		return NULL;
+	}
+	strcpy(ircsock->chan, chan);
+	
+	return ircsock;
+}
+
+void ircsock_free(IRCSock *ircsock) {
+	if(!ircsock)
+		return;
+	free(ircsock->host);
+	free(ircsock->nick);
+	free(ircsock->chan);
+	free(ircsock);
+}
 
--- ircsock.h
+++ ircsock.h
@@ -4,7 +4,16 @@
 #include "cbuffer.h"
 
 typedef struct {
+	char *host;
+	int port;
+
+	char *nick;
+	char *chan;
+
 	CBuffer *cbuf;
 } IRCSock;
+
+IRCSock *ircsock_create(char *host, int port, char *nick, char *chan);
+void ircsock_free(IRCSock *ircsock);
 
 #endif /* IRCSOCK_H */
