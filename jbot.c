b4a879216b3aa733e370306bf29af33b425f7146
Jeff Chapman II <jac@JAChapmanII.net>
2011-09-24 15:23:49 -0400

    Spawn second thread to loop _read

--- jbot.c
+++ jbot.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "ircsock.h"
 
 #define SERVER ".slashnet.org"
 
 IRCSock *ircSocket = NULL;
 
+void *readLoop(void *args) {
+	if(args != NULL)
+		return NULL;
+	while(1) {
+		ircsock_read(ircSocket);
+	}
+}
+
 int main(int argc, char **argv) {
+	char *nick = "jbotc", *chan = "#zebra";
 	char *prefix = "irc", *str, *sname;
 	int port = 6667;
-	char *nick = "jbotc", *chan = "#zebra";
+	pthread_t readThread;
+
 	if(argc > 1)
 		prefix = argv[1];
 
@@ -34,10 +45,13 @@ int main(int argc, char **argv) {
 	printf("Joining %s as %s...\n", chan, nick);
 	ircsock_join(ircSocket);
 
+	printf("Creating read thread...\n");
+	pthread_create(&readThread, NULL, readLoop, NULL);
+
 	printf("Main loop\n");
 	while(1) {
-		ircsock_read(ircSocket);
 		while((str = cbuffer_pop(ircSocket->cbuf)) != NULL) {
+			printf("%s\n", str);
 			if((str[0] == 'P') && (str[1] == 'I') &&
 				(str[2] == 'N') && (str[3] == 'G') &&
 				(str[4] == ' ') && (str[5] == ':')) {
