cbe772054d3370117d1630a5e2e5d90d6715267f
Jeff Chapman II <jac@JAChapmanII.net>
2011-11-03 11:59:59 -0400

    More refactoring. See full commit message
    More work has been done in order to make the C more modern. More (nearly all)
    	comments which should be single line are now single line.
    Some types have been changed (style wise only). "char*" has been changed to
    	"char *" to make the fact that * binds to the name more obvious.
    if/else blocks have been standardized back to the way they were.
    Space only lines have been emptied.
    What the join/privmsg regexs actually are has been made more obvious.
    Minor change to log output about PRIVMSG-es

--- jbot.c
+++ jbot.c
@@ -70,7 +70,7 @@ void markov(const char *name, char *tok) { // {{{
 } // }}}
 
 /* Declares variables to remember things. */
-void declareVariable(const char* name, char* tok) {
+void declareVariable(const char *name, char *tok) {
 	BMap_Node *tmpn = NULL;
 
 	tok = strtok(NULL, " ");
@@ -82,54 +82,49 @@ void declareVariable(const char* name, char* tok) {
 			bmap_add(variableMap, tok, "0");
 			send(chan, "%s: set \"%s\" to 0", name, tok);
 		}
-	} 
-    else {
-	    send(chan, "%s: \"%s\" is %s", name, tok, tmpn->val);
+	} else {
+		send(chan, "%s: \"%s\" is %s", name, tok, tmpn->val);
 	}
 }
 
 /* Set a variable to remember things. */
-void setVariable(const char* name, char* tok) {
+void setVariable(const char *name, char *tok) {
 	BMap_Node *tmpn = NULL;
-	char* tmpsp;
-	
+	char *tmpsp;
+
 	tok = strtok(NULL, " ");
 	tmpsp = tok;
 	tok = strtok(NULL, " ");
 	if(!tok || !tmpsp) {
 		send(chan, "%s: You must specify a variable and value", name);
-	} 
-	else {
+	} else {
 		bmap_add(variableMap, tmpsp, tok);
 		tmpn = bmap_find(variableMap, tmpsp);
 		if(!tmpn) {
 			send(chan, "%s: \"%s\" was not found!?", name, tmpsp);
-		} 
-		else {
+		} else {
 			send(chan, "%s: \"%s\" is %s", name, tmpsp, tmpn->val);
 		}
-	}	
+	}
 }
 
 /* Increment a variable and create it if it doesn't exist. */
-void incrementVariable(const char* name, char* tok) {
+void incrementVariable(const char *name, char *tok) {
 	BMap_Node *tmpn = NULL;
-	
+
 	tok = strtok(NULL, " ");
 	tmpn = bmap_find(variableMap, tok);
 	if(tmpn == NULL) {
 		if(bmap_size(variableMap) >= 256) {
 			send(chan, "%s: 256 variables exist already, sorry!", name);
-		} 
-        else {
+		} else {
 			bmap_add(variableMap, tok, "0");
 			send(chan, "%s: set \"%s\" to 0", name, tok);
 		}
-	} 
-	else {
+	} else {
 		int tmp;
 		char tmps[PBSIZE];
-		
+
 		tmp = atoi(tmpn->val);
 		++tmp;
 		snprintf(tmps, PBSIZE, "%d", tmp);
@@ -139,24 +134,22 @@ void incrementVariable(const char* name, char* tok) {
 }
 
 /* Decrement a variable and create it if it doesn't exist. */
-void decrementVariable(const char* name, char* tok) {
+void decrementVariable(const char *name, char *tok) {
 	BMap_Node *tmpn = NULL;
-	
+
 	tok = strtok(NULL, " ");
 	tmpn = bmap_find(variableMap, tok);
 	if(tmpn == NULL) {
 		if(bmap_size(variableMap) >= 256) {
 			send(chan, "%s: 256 variables exist already, sorry!", name);
-		}
-		else {
+		} else {
 			bmap_add(variableMap, tok, "0");
 			send(chan, "%s: set \"%s\" to 0", name, tok);
 		}
-	} 
-	else {
+	} else {
 		int tmp;
 		char tmps[PBSIZE];
-		
+
 		tmp = atoi(tmpn->val);
 		tmp--;
 		snprintf(tmps, PBSIZE, "%d", tmp);
@@ -169,54 +162,55 @@ void decrementVariable(const char* name, char* tok) {
  * everything to a file name *lfname. See internals for commands recognized
  */
 int main(int argc, char **argv) {
-
 	char str[BSIZE], *tok, *tmsg, *cstart;
 	char name[PBSIZE], hmask[PBSIZE], cname[PBSIZE], msg[BSIZE];
-	
+
 	regex_t pmsgRegex, joinRegex;
 	regmatch_t mptr[16];
 	int res, done = 0, r, toUs;
 
-	/* seed random number generator with current time */
+	// TODO: abuse macros? stringification/concatenation?
+	const char *privmsgRegexExp =
+		"^:([A-Za-z0-9_]*)!([-@~A-Za-z0-9_\\.]*) PRIVMSG ([#A-Za-z0-9_]*) :(.*)";
+	const char *joinRegexExp = 
+		"^:([A-Za-z0-9_]*)!([-@~A-Za-z0-9_\\.]*) JOIN :([#A-Za-z0-9_]*)";
+
+	// seed random number generator with current time
 	srand(time(NULL));
 
-	/* If we fail to compile the PRIVMSG regex, abort */
-	res = regcomp(&pmsgRegex,
-			"^:([A-Za-z0-9_]*)!([-@~A-Za-z0-9_\\.]*) PRIVMSG ([#A-Za-z0-9_]*) :(.*)",
-			REG_EXTENDED);
+	// If we fail to compile the PRIVMSG regex, abort
+	res = regcomp(&pmsgRegex, privmsgRegexExp, REG_EXTENDED);
 	if(res) {
 		fprintf(stderr, "Could not compile privmsg regex!\n");
 		fprintf(stderr, "erromsg: %s\n", getRegError(res, &pmsgRegex));
 		return 1;
 	}
 
-	/* If we fail to compile the JOIN regex, abort */
-	res = regcomp(&joinRegex,
-			"^:([A-Za-z0-9_]*)!([-@~A-Za-z0-9_\\.]*) JOIN :([#A-Za-z0-9_]*)",
-			REG_EXTENDED);
+	// If we fail to compile the JOIN regex, abort
+	res = regcomp(&joinRegex, joinRegexExp, REG_EXTENDED);
 	if(res) {
 		fprintf(stderr, "Could not compile join regex!\n");
 		fprintf(stderr, "erromsg: %s\n", getRegError(res, &joinRegex));
 		return 1;
 	}
 
-	/* If we fail to create a basic map, abort */
+	// If we fail to create a basic map, abort
 	if((variableMap = bmap_create()) == NULL) {
 		fprintf(stderr, "Could not create contstant map!\n");
 		return 1;
 	}
 
-	/* If we fail to open the logFile in append mode, abort */
+	// If we fail to open the logFile in append mode, abort
 	logFile = fopen(lfname, "a");
 	if(!logFile) {
 		fprintf(stderr, "Could not open logFile file!\n");
 		return 1;
 	}
 
-	/* print separator to logFile */
+	// print separator to logFile
 	fprintf(logFile, "------------------------------------\n");
 
-	/* Setup the command start string, abort on failure */
+	// Setup the command start string, abort on failure
 	cstart = malloc(strlen(nick) + 2);
 	if(!cstart) {
 		fprintf(stderr, "Could not malloc space for command start string!\n");
@@ -229,91 +223,88 @@ int main(int argc, char **argv) {
 	fflush(stdout);
 	fflush(logFile);
 
-	/* main loop, go until we say we are done or parent is dead/closes us off */
+	// main loop, go until we say we are done or parent is dead/closes us off
 	while(!feof(stdin) && !done) {
 		if(fgets(str, BSIZE - 1, stdin) == str) {
-			/* replace newline with null */
+			// replace newline with null
 			str[strlen(str) - 1] = '\0';
 
-			/* logFile input */
+			// logFile input
 			fprintf(logFile, " <- %s\n", str);
 			res = regexec(&pmsgRegex, str, pmsgRegex.re_nsub + 1, mptr, 0);
-			/* if a PRIVMSG was broadcast to us */
+			// if a PRIVMSG was broadcast to us
 			if(res == 0) {
-				/* copy nick of sender into name */
+				// copy nick of sender into name
 				strncpy(name, str + mptr[1].rm_so, mptr[1].rm_eo - mptr[1].rm_so);
 				name[mptr[1].rm_eo - mptr[1].rm_so] = '\0';
 
-				/* copy host mask of sender into hmask */
+				// copy host mask of sender into hmask
 				strncpy(hmask, str + mptr[2].rm_so, mptr[2].rm_eo - mptr[2].rm_so);
 				hmask[mptr[2].rm_eo - mptr[2].rm_so] = '\0';
 
-				/* copy target of PRIVMSG into cname */
+				// copy target of PRIVMSG into cname
 				strncpy(cname, str + mptr[3].rm_so, mptr[3].rm_eo - mptr[3].rm_so);
 				cname[mptr[3].rm_eo - mptr[3].rm_so] = '\0';
 
-				/* copy message part into msg */
+				// copy message part into msg
 				strcpy(msg, str + mptr[4].rm_so);
-				/* setup tmsg to point to the msg in str */
+				// setup tmsg to point to the msg in str
 				tmsg = str + mptr[4].rm_so;
 
-				fprintf(logFile, "PRIVMSG %s :PRIVMSG recieved from %s@%s: %s\n",
-						owner, name, cname, msg);
+				fprintf(logFile, "PRIVMSG from %s to %s: %s\n", name, cname, msg);
 				tok = strtok(tmsg, " ");
 
 				toUs = 0;
-				/* if this is targeted at us */
+				// if this is targeted at us
 				if(!strcmp(tok, cstart)) {
-					/* ignore it */
+					// ignore it
 					tok = strtok(NULL, " ");
 					toUs = 1;
 				}
 
-				/* reload stops this instance, parent conbot starts new one */
+				// reload stops this instance, parent conbot starts new one
 				if(!strcmp(tok, "reload") && !strcmp(name, owner) && toUs) {
 					fprintf(logFile, "Got message to restart...\n");
 					done = 1;
+				// markov prints markov chains generated from previous input
+				} else if(!strcmp(tok, "markov")) {
+					markov(name, tok);
 				} 
-				/* markov will eventually print markov chains generated from
-				 * previous input. TODO: implement */
-                else if(!strcmp(tok, "markov")) {
-                    markov(name, tok);
-				} 
-				/* CodeBlock wants a fish... */
+				// CodeBlock wants a fish...
                 else if(!strcmp(tok, "fish")) {
 					r = rand();
 					send(chan, "%s: %s", name, ((r % 2) ? "><>" : "<><"));
 				} 
-				/* CodeBlock wants multiple species of fish */
+				// CodeBlock wants multiple species of fish
                 else if(!strcmp(tok, "fishes")) {
 					send(chan, "%s: ><> <>< <><   ><> ><>", name);
 				} 
-				/* WUB WUB WUB WUB WUB */
+				// WUB WUB WUB WUB WUB
                 else if(!strcmp(tok, "dubstep")) {
 					send(chan, "%s: WUB WUB WUB", name);
 				} 
-				/* declaring a variable */
+				// declaring a variable
                 else if(!strcmp(tok, "declare")) {
 					declareVariable(name, tok);
 				} 
-				/* setting a variable */
+				// setting a variable
                 else if(!strcmp(tok, "set")) {
 					setVariable(name, tok);
 				} 
-				/* incrementing a variable (or declaring it) */
+				// incrementing a variable (or declaring it)
                 else if(!strcmp(tok, "inc") || !strcmp(tok, "increment") ||
 						!strcmp(tok, "++")) {
 					incrementVariable(name, tok);
 				} 
-				/* decrementing a variable (or declaring it) */
+				// decrementing a variable (or declaring it)
                 else if(!strcmp(tok, "dec") || !strcmp(tok, "decrement") ||
 						!strcmp(tok, "--")) {
 					decrementVariable(name, tok);
 				} 
-				/* token after cstart does not match command */
+				// token after cstart does not match command
                 else {
 					if(toUs) {
-						/* msg ends with question mark, guess an answer */
+						// msg ends with question mark, guess an answer
 						if((strlen(msg) > 0) && (msg[strlen(msg) - 1] == '?')) {
 							r = rand();
 							send(chan, "%s: %s", name, ((r % 2) ? "Yes" : "No"));
@@ -322,17 +313,17 @@ int main(int argc, char **argv) {
 				}
 			}
 			res = regexec(&joinRegex, str, joinRegex.re_nsub + 1, mptr, 0);
-			/* if a JOIN was broadcast to us */
+			// if a JOIN was broadcast to us
 			if(res == 0) {
-				/* copy nick of sender into name */
+				// copy nick of sender into name
 				strncpy(name, str + mptr[1].rm_so, mptr[1].rm_eo - mptr[1].rm_so);
 				name[mptr[1].rm_eo - mptr[1].rm_so] = '\0';
 
-				/* copy host mask of sender into hmask */
+				// copy host mask of sender into hmask
 				strncpy(hmask, str + mptr[2].rm_so, mptr[2].rm_eo - mptr[2].rm_so);
 				hmask[mptr[2].rm_eo - mptr[2].rm_so] = '\0';
 
-				/* copy target of PRIVMSG into cname */
+				// copy target of PRIVMSG into cname
 				strncpy(cname, str + mptr[3].rm_so, mptr[3].rm_eo - mptr[3].rm_so);
 				cname[mptr[3].rm_eo - mptr[3].rm_so] = '\0';
 
@@ -341,18 +332,18 @@ int main(int argc, char **argv) {
 					send(chan, "%s: %s", name, obtainGreeting());
 					*/
 			}
-			/* flush everything so output goes out immediately */
+			// flush everything so output goes out immediately
 			fflush(stdout);
 			fflush(logFile);
 		} 
-		/* fgets failed, handle printing error message */
+		// fgets failed, handle printing error message
         else {
 			fprintf(stderr, "fgets failed in main jbot loop!\n");
 			fprintf(logFile, "fgets failed in main jbot loop!\n");
 		}
 	}
 
-	/* close stdin/stdout just to make sure conbot knows we stopped */
+	// close stdin/stdout just to make sure conbot knows we stopped
 	close(0);
 	close(1);
 	return 0;
