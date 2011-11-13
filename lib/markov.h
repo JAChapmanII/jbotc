2b9e3f4fe3b7e3f8511a3465f827ee3ac782a4ba
Jeff Chapman II <jac@JAChapmanII.net>
2011-11-13 08:06:53 -0500

    Add another function prototype, add some comments

--- lib/markov.h
+++ lib/markov.h
@@ -11,7 +11,19 @@ typedef struct {
 Markov *markov_create(int order);
 void markov_free(Markov *mkv);
 
-void  markov_insert(Markov *mkv, char *str);
+/* Use this to tokenize and insert a string of arbitrary length */
+void markov_insert(Markov *mkv, char *str);
+/* Internal helper function, inserts one specific element
+ * 	words is a markov->order + 1 char*'s which are the input and output
+ */
+void markov_push(Markov *mkv, char **words);
+
+/* Use this to fetch some variable length output from a seed
+ * 	note: seed must be either
+ * 		exactly markov->order words long
+ * 		exactly markov->order -1 words long with a starting space */
+char *markov_fetch(Markov *mkv, char *seed, int maxLength);
+/* Fetch one word using some input */
 char *markov_search(Markov *mkv, char *str);
 
 #endif // MARKOV_H
